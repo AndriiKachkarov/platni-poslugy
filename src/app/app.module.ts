4dffc074e86b1ec6046ccfe88039b46162b35432
Andrii Kachkarov <andriikachkarov@gmail.com>
2020-08-26 11:27:08 +0300

    Add the MainLayoutComponent and its internal components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
 import {LoginComponent} from './login/login.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { ClientComponent } from './main-layout/client/client.component';
+import { InvoiceComponent } from './main-layout/invoice/invoice.component';
+import { MainPageComponent } from './main-layout/main-page/main-page.component';
+import { CertificationComponent } from './main-layout/main-page/certification/certification.component';
+import { HeaderComponent } from './main-layout/main-page/header/header.component';
+import { MonitoringComponent } from './main-layout/main-page/monitoring/monitoring.component';
+import { ServicePacksComponent } from './main-layout/main-page/service-packs/service-packs.component';
+import { TableComponent } from './main-layout/main-page/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainLayoutComponent,
+    ClientComponent,
+    InvoiceComponent,
+    MainPageComponent,
+    CertificationComponent,
+    HeaderComponent,
+    MonitoringComponent,
+    ServicePacksComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-layout/main-page/service-packs/service-packs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-packs',
+  templateUrl: './service-packs.component.html',
+  styleUrls: ['./service-packs.component.scss']
+})
+export class ServicePacksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
