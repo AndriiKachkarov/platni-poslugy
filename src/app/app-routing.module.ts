c1bb42339fdf021c1f75a86d144f73f171826b6a
Andrii Kachkarov <andriikachkarov@gmail.com>
2020-08-26 11:32:53 +0300

    Edit routing settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainLayoutComponent} from './main-layout/main-layout.component';
+import {MainPageComponent} from './main-layout/main-page/main-page.component';
+import {NotFoundComponent} from './shared/components/not-found/not-found.component';
+import {InvoiceComponent} from './main-layout/invoice/invoice.component';
+import {LoginComponent} from './login/login.component';
+import {AuthGuard} from './services/auth.guard';
+import {ClientComponent} from './main-layout/client/client.component';
+import {PreviousRouteRecorderService} from './services/previous-route-recorder.service';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {
+    path: '', component: MainLayoutComponent, canActivate: [AuthGuard],  children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: MainPageComponent, canDeactivate: [PreviousRouteRecorderService]},
+      {path: 'invoice', component: InvoiceComponent, canDeactivate: [PreviousRouteRecorderService]},
+      {path: 'client', component: ClientComponent, canDeactivate: [PreviousRouteRecorderService]},
+    ]
+  },
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
