6c29ce9669ad4f4430b401e3c6fe50e5196f9764
Andrii Kachkarov <andriikachkarov@gmail.com>
2020-08-26 11:59:43 +0300

    Edit app.module.ts, finish ClientComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {LOCALE_ID, NgModule, Provider} from '@angular/core';
+import localeRu from '@angular/common/locales/ru';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,20 @@ import { HeaderComponent } from './main-layout/main-page/header/header.component
 import { MonitoringComponent } from './main-layout/main-page/monitoring/monitoring.component';
 import { ServicePacksComponent } from './main-layout/main-page/service-packs/service-packs.component';
 import { TableComponent } from './main-layout/main-page/table/table.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {AuthInterceptor} from './shared/auth.interceptor';
+import {environment} from '../environments/environment';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localeRu, 'ru');
+
+const INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  multi: true,
+  useClass: AuthInterceptor
+};
 
 @NgModule({
   declarations: [
@@ -32,9 +47,14 @@ import { TableComponent } from './main-layout/main-page/table/table.component';
   imports: [
     BrowserModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
   ],
-  providers: [],
+  providers: [
+    INTERCEPTOR_PROVIDER,
+    { provide: LOCALE_ID, useValue: 'ru' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/main-layout/client/client.component.ts
+++ src/app/main-layout/client/client.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ClientService} from '../../services/client.service';
+import {Client} from '../../shared/interfaces';
 
 @Component({
   selector: 'app-client',
@@ -7,9 +11,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  submitted = false;
+  message: string;
+
+  constructor(
+    private  router: Router,
+    private  clientService: ClientService,
+    private route: ActivatedRoute
+  ) { }
+
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((param) => {
+      if (param['client']) {
+
+      }
+      this.form = new FormGroup({
+        name: new FormControl(this.clientService.client ? this.clientService.client.name : null, [Validators.required]),
+        EDRPOU: new FormControl((this.clientService.client && this.clientService.client.EDRPOU) ? this.clientService.client.id : null, [],
+          this.ExistedClient.bind(this)
+        ),
+        address: new FormControl((this.clientService.client && this.clientService.client.address) ? this.clientService.client.address : null),
+        additions: new FormControl((this.clientService.client && this.clientService.client.additions) ? this.clientService.client.additions : null)
+      });
+    });
+
+  }
+  submit(event){
+    console.log('test');
+    if ( this.form.invalid) {
+      return;
+    }
+    this.submitted = true;
+    if (this.clientService.client && this.clientService.client.id) {
+      const id = this.clientService.client.id;
+      this.clientService.client = this.form.value;
+      this.clientService.client.id = id;
+      this.clientService.patch().subscribe((res) => {
+        this.submitted = false;
+        this.router.navigate(['/']);
+      });
+    } else {
+      this.clientService.client = this.form.value;
+      this.clientService.create().subscribe((res) => {
+        this.submitted = false;
+        this.router.navigate(['/invoice']);
+      });
+    }
   }
 
+  return() {
+    const url = localStorage.getItem('previousRoute');
+    if (url) {
+      this.router.navigate([url]);
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
+
+  ExistedClient(control: FormControl): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.clientService.getClientByEDRPOU( control.value).subscribe((client: Client) => {
+        if (client) {
+          resolve({forbiddenEmail: true});
+        } else {
+          resolve(null);
+        }
+      });
+    });
+  }
 }
