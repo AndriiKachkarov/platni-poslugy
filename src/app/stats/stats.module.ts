c3bd989ae4cfb60bf7851f985949bcece3789714
Andrii Kachkarov <andriikachkarov@gmail.com>
2020-09-17 10:08:46 +0300

    Add routing for StatsModule

--- src/app/stats/stats.module.ts
+++ src/app/stats/stats.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatsComponent } from './components/stats/stats.component';
+import {RouterModule} from '@angular/router';
 
 
 
 @NgModule({
   declarations: [StatsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {path: '', component: StatsComponent}
+    ])
   ]
 })
 export class StatsModule { }
