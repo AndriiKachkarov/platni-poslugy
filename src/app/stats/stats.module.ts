3a407ae9f3dc0654f6d76943ebd627e9cae930a9
Andrii Kachkarov <andriikachkarov@gmail.com>
2020-09-17 09:56:36 +0300

    Add StatsComponent

--- src/app/stats/stats.module.ts
+++ src/app/stats/stats.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StatsComponent } from './components/stats/stats.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [StatsComponent],
   imports: [
     CommonModule
   ]
