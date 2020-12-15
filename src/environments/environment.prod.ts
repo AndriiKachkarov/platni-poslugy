e36514d5bfbeecdabe0f59b1562f677c031740b3
AndriyKachkarov <Andriy.Kachkarov@gmail.com>
2020-12-15 15:42:22 +0200

    fix little issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import {Environment} from './interface';
 
 export const environment: Environment = {
-  production: true ,
+  production: true,
   apiKey: 'AIzaSyADFmpYi98PU-xBFdFddUOSWSxlJZ34_rI',
   fbDbUrl: 'https://platni-poslugy.firebaseio.com',
   fbDbInvoicesCollection: 'invoices'
